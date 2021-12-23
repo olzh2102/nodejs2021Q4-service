2b8f98ffd1a245b31c9a0d4b40cf527cba189e51
Olzhas Kurikov <okur@hellofresh.com>
2021-12-24 00:30:14 +0100

    feat: log uncaughtException and unhandledRejection

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,14 +1,29 @@
 import pino from 'pino';
 
-const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      destination: './logger.log',
-      translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
-      ignore: 'pid,hostname',
+const transport = pino.transport({
+  targets: [
+    {
+      level: 'error',
+      target: 'pino-pretty',
+      options: {
+        destination: './logs/error.log',
+        mkdir: true,
+        translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+        ignore: 'pid,hostname',
+      },
     },
-  },
+    {
+      level: 'trace',
+      target: 'pino-pretty',
+      options: {
+        destination: './logs/all.log',
+        mkdir: true,
+        translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+        ignore: 'pid,hostname',
+      },
+    },
+  ],
 });
 
+const logger = pino(transport);
 export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import pino from 'pino';
+
 import build from './app';
 import config from './common/config';
 // import logger from './logger';
@@ -12,6 +14,17 @@ app.listen(config.PORT, (err: Error | null) => {
   }
 });
 
-// TODO
-process.on('uncaughtException', () => {});
-process.on('unhandledRejection', () => {});
+const handler = pino.final(logger, (err, finalLogger, evt) => {
+  finalLogger.info(`${evt} caught`);
+  if (err) finalLogger.error(err, 'error caused exit');
+  process.exit(err ? 1 : 0);
+});
+
+process.on('uncaughtException', (err) => handler(err, 'uncaughtException'));
+process.on(
+  'unhandledRejection',
+  pino.final(logger, (err, finalLogger) => {
+    finalLogger.error(err, 'unhandledRejection');
+    process.exit(1);
+  })
+);
