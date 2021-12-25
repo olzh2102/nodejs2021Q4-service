5e8a9e14b636279520278ccc87a874a5ad547cc4
Olzhas Kurikov <okur@hellofresh.com>
2021-12-25 20:27:18 +0100

    refactor: add log PORT, remove fastify logger

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import logger from './logger/logger';
 const app = build({ logger });
 
 app.listen(config.PORT, (err: Error | null) => {
+  console.log(`Server running on port: ${config.PORT}`);
   if (err) {
     app.log.error(err);
     process.exit(1);
